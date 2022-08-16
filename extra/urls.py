7d9299bf5734c1a3e163eab8686e3f9e7f3c8009
salman_khosravipour <khosravipour.salman@gmail.com>
2022-08-16 17:14:34 +0430

    add search

--- extra/urls.py
+++ extra/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('tag-list/', views.tag_list, name='tag_list')
+	path('tag-list/', views.tag_list, name='tag_list'),
+	path('tag-search/', views.tag_search, name='tag_search'),
 ]
--- extra/views.py
+++ extra/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 from .models import TagModel
+from .forms import TagSearchForm
 
 # Create your views here.
 
 def tag_list(request):
-	tags = TagModel.objects.all()
-	return render(request, 'extra/tag_list.html', {'tags':tags})
+    tags = TagModel.objects.all()
+    return render(request, 'extra/tag_list.html', {'tags':tags})
+
+def tag_search(request):
+    form = TagSearchForm()
+
+    if request.method == 'POST':
+        form = TagSearchForm(request.POST)
+        if form.is_valid():
+            tag_name = form.cleaned_data.get('name')
+            tag_list = TagModel.objects.filter(name__icontains=tag_name)
+
+            context = {'tags':tag_list} 
+            return render(request, "extra/tag_list.html", context)
+
+    return render(request, "extra/tag_list.html", {'form':form})
